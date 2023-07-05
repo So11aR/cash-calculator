468aca0ddb085741775091a4b6da9bb3e736e71f
Sergey <s.spiridonovv@mail.ru>
2023-07-05 15:35:06 +0300

    test-data script

--- js/controller.js
+++ js/controller.js
@@ -1,10 +1,19 @@
 let controller = (function(budgetCtrl, uiCtrl) {
+
+  let DOM = uiCtrl.getDomStrings()
+  console.log(DOM);
   
+  // функция срабатывает при отрпавке формы
   function ctrlAddItem(event) {
     event.preventDefault()
     console.log('Fired!');
+
+    let input = uiCtrl.getInput()
+    console.log(input);
+
+    
   }
 
-  document.querySelector('#budget-form').addEventListener('submit', ctrlAddItem)
+  document.querySelector(DOM.form).addEventListener('submit', ctrlAddItem)
 
 })(modelController, viewController)
--- js/view.js
+++ js/view.js
@@ -1,3 +1,23 @@
 let viewController = (function() {
   
+  let DOMstrings = {
+    form: '#budget-form',
+    inputType: '#input__type',
+    inputDescription: '#input__description',
+    inputValue: '#input__value'
+  }
+
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      }
+    },
+    getDomStrings: function() {
+      return DOMstrings
+    }
+  }
+
 })()
