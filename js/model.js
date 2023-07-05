a8180dfc91c536bf79416228a152b936a0424b61
Sergey <s.spiridonovv@mail.ru>
2023-07-05 15:43:40 +0300

    model data

--- js/model.js
+++ js/model.js
@@ -1,3 +1,26 @@
 let modelController = (function() {
   
+  let Income = function(id, description, value) {
+    this.id = id
+    this.description = description
+    this.value = value
+  }
+
+  let Expense = function(id, description, value) {
+    this.id = id
+    this.description = description
+    this.value = value
+  }
+
+  let data = {
+    allItems: {
+      inc: [],
+      exp: []
+    },
+    totals: {
+      inc: 0,
+      exp: 0
+    }
+  }
+
 })()
